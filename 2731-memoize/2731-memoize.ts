8480f3f89bdcdd6b725617a060346e5f6c2beab6
Charles <76520716+KwonCheulJin@users.noreply.github.com>
2025-07-30 12:33:28 +0900

    Time: 235 ms (96.06%), Space: 95.3 MB (81.92%) - LeetHub

---
+++ 2731-memoize/2731-memoize.ts
@@ -0,0 +1,27 @@
+type Fn = (...params: number[]) => number
+
+function memoize(fn: Fn): Fn {
+    const memo = new Map()
+    return function(...args) {
+        const key = JSON.stringify(args);
+        if (memo.has(key)) {
+            return memo.get(key)
+        } else {
+            const answer = fn(...args)
+            memo.set(key, answer)
+            return answer
+        }
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */

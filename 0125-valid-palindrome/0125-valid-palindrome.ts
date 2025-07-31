1596418f11afca444b2f1c42de8897830ec617aa
Charles <76520716+KwonCheulJin@users.noreply.github.com>
2025-07-31 15:50:52 +0900

    Time: 5 ms (60.46%), Space: 58.5 MB (70.89%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -0,0 +1,17 @@
+function isPalindrome(s: string): boolean {
+    const cleanedStr = s.toLowerCase().replace(/[^a-z0-9]/g, '')
+
+    let left = 0
+    let right = cleanedStr.length - 1;
+
+    while(left < right) {
+        if (cleanedStr[left] !== cleanedStr[right]) {
+            return false;
+        }
+
+        left++
+        right--
+    }
+
+    return true
+};

6e13ffc283a369563aa314d6592264a214d2a730
Charles <76520716+KwonCheulJin@users.noreply.github.com>
2025-07-31 17:03:40 +0900

    Time: 3 ms (65.84%), Space: 57.6 MB (24.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,13 +1,11 @@
 function twoSum(nums: number[], target: number): number[] {
-    const complement = new Map()
+    const saveNums = new Map()
 
     for (let i = 0; i < nums.length; i++) {
-        const num = nums[i]
-
-        if (complement.has(num)) {
-            return [complement.get(num), i]
+        let num = nums[i]
+        if (saveNums.has(num)) {
+            return [saveNums.get(num), i]
         }
-
-        complement.set(target-num, i)
+        saveNums.set(target - num, i)
     }
 };

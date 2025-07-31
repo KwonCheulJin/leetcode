e28ccc6ef898dfef702560edcb22f371b7701291
Charles <76520716+KwonCheulJin@users.noreply.github.com>
2025-07-31 15:06:34 +0900

    Time: 3 ms (55.34%), Space: 65.8 MB (58.82%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,10 @@
+function maxProfit(prices: number[]): number {
+    let minPrice = prices[0]
+    let profit = 0
+    for (let i = 0; i < prices.length; i++) {
+        minPrice = Math.min(minPrice, prices[i])
+        profit = Math.max(profit, prices[i] - minPrice)
+    }
+
+    return profit
+};

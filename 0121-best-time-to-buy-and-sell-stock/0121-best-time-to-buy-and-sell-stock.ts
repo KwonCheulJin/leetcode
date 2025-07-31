40b01fe1b46d2932e80a5d93082e29b3bf90ae40
Charles <76520716+KwonCheulJin@users.noreply.github.com>
2025-07-31 15:07:19 +0900

    Time: 0 ms (100%), Space: 64.9 MB (94.87%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,7 +1,7 @@
 function maxProfit(prices: number[]): number {
     let minPrice = prices[0]
     let profit = 0
-    for (let i = 0; i < prices.length; i++) {
+    for (let i = 1; i < prices.length; i++) {
         minPrice = Math.min(minPrice, prices[i])
         profit = Math.max(profit, prices[i] - minPrice)
     }

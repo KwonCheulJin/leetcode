33ed43b6d88390c2e1ac557e3935ec53fd269b4d
Charles <76520716+KwonCheulJin@users.noreply.github.com>
2025-07-31 21:46:34 +0900

    Time: 54 ms (32.8%), Space: 54.9 MB (68.1%) - LeetHub

--- 2622-cache-with-time-limit/2622-cache-with-time-limit.ts
+++ 2622-cache-with-time-limit/2622-cache-with-time-limit.ts
@@ -1,35 +1,46 @@
-interface TTLValue {
-    value: number;
-    timer: ReturnType<typeof setTimeout>;
-}
-
 class TimeLimitedCache {
-    private cache = new Map<number, TTLValue>()
-    
-    set(key: number, value: number, duration: number): boolean {
-        const exists = this.cache.has(key);
-        if (exists) {
-            clearTimeout(this.cache.get(key).timer);
-        }
-        this.cache.set(key, {
-            value,
-            timer: setTimeout(() => this.cache.delete(key), duration)
-        })
-        return exists
-    }
-    
-    get(key: number): number {
-        return this.cache.has(key) ? this.cache.get(key).value : -1;
-    }
-    
-    count(): number {
-        return this.cache.size;
-    }
-}
+  private cache: Map<number, { value: number; expire: number }>;
 
-/**
- * const timeLimitedCache = new TimeLimitedCache()
- * timeLimitedCache.set(1, 42, 1000); // false
- * timeLimitedCache.get(1) // 42
- * timeLimitedCache.count() // 1
- */
+  constructor() {
+    this.cache = new Map();
+  }
+
+  set(key: number, value: number, duration: number): boolean {
+    const now = Date.now();
+    const isExistingAndUnexpired =
+      this.cache.has(key) && this.cache.get(key)!.expire > now;
+
+    this.cache.set(key, {
+      value,
+      expire: now + duration,
+    });
+
+    return !!isExistingAndUnexpired;
+  }
+
+  get(key: number): number {
+    const now = Date.now();
+    const entry = this.cache.get(key);
+
+    if (!entry || entry.expire <= now) {
+      return -1;
+    }
+
+    return entry.value;
+  }
+
+  count(): number {
+    const now = Date.now();
+    let validCount = 0;
+
+    for (const [key, { expire }] of this.cache.entries()) {
+      if (expire > now) {
+        validCount++;
+      } else {
+        this.cache.delete(key); // 만료된 키는 정리
+      }
+    }
+
+    return validCount;
+  }
+}
